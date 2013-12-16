94c3ed757de5d14283032350a0d0b0f674fb585a
Aequiternus <aequiternus@gmail.com>
2013-12-17 00:05:14 +0400

    new themes

--- min.sh
+++ min.sh
@@ -8,14 +8,14 @@ yui-compressor adefault.css > adefault.min.css
 
 echo Prepare themes
 cp adefault.min.css adefault-light.min.css
-cp adefault.min.css adefault-light-soft.min.css
+cp adefault.min.css adefault-lighter.min.css
 cp adefault.min.css adefault-dark.min.css
-cp adefault.min.css adefault-dark-bright.min.css
+cp adefault.min.css adefault-darker.min.css
 
 echo Compress themes
-yui-compressor adefault-light.css       | sed 's/@import "adefault.css";//' >> adefault-light.min.css
-yui-compressor adefault-light-soft.css  | sed 's/@import "adefault.css";//' >> adefault-light-soft.min.css
-yui-compressor adefault-dark.css        | sed 's/@import "adefault.css";//' >> adefault-dark.min.css
-yui-compressor adefault-dark-bright.css | sed 's/@import "adefault.css";//' >> adefault-dark-bright.min.css
+yui-compressor adefault-light.css   | sed 's/@import "adefault.css";//' >> adefault-light.min.css
+yui-compressor adefault-lighter.css | sed 's/@import "adefault.css";//' >> adefault-lighter.min.css
+yui-compressor adefault-dark.css    | sed 's/@import "adefault.css";//' >> adefault-dark.min.css
+yui-compressor adefault-darker.css  | sed 's/@import "adefault.css";//' >> adefault-darker.min.css
 
 echo Done
